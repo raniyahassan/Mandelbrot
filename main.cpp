c8d0f100cfe2cf9c3797b45819c94af98aa9118f
admin <admin@BCS-Ubuntu-01.lrccd.ad.losrios.edu>
2022-09-26 13:29:41 -0700

    ADD

--- main.cpp
+++ main.cpp
@@ -3,14 +3,22 @@
 #include <ComplexPlane.h>
 
 using namespace std;
-
-int width = sf::VideoMode::getDesktopMode().width;
-int height = sf::VideoMode::getDesktopMode().height;
-
-int aspectRatio = width/height; 
+using namespace sf;
 
 int main()
 
 {
+    int width = sf::VideoMode::getDesktopMode().width;
+    int height = sf::VideoMode::getDesktopMode().height;
+    int aspectRatio = width/height; 
+
+    VideoMode VideoWindow(width, height); 
+    RenderWindow window(VideoWindow,"Mandelbrot", Style::Default);
+
+
+
+
+
+
     return 0; 
 }
