2d3f2f56595ef4f882650a75bd24744073a56d9a
admin <admin@BCS-Ubuntu-06.lrccd.ad.losrios.edu>
2022-09-30 06:00:07 -0700

    changes

--- main.cpp
+++ main.cpp
@@ -5,16 +5,50 @@
 using namespace std;
 using namespace sf;
 
+int width = sf::VideoMode::getDesktopMode().width;
+int height = sf::VideoMode::getDesktopMode().height;
+int aspectRatio = height/width; 
+
+VideoMode VideoWindow(width, height); 
+RenderWindow window(VideoWindow,"Mandelbrot", Style::Default);
+
+ComplexPlane complexPlane();
+Font font; 
+Text text; 
+VertexArray Colors(Points, height*width);
+
+enum State { CALCULATING, DISPLAYING }; 
+State current = CALCULATING; 
+
+Event event; 
+
 int main()
-
 {
-    int width = sf::VideoMode::getDesktopMode().width;
-    int height = sf::VideoMode::getDesktopMode().height;
-    int aspectRatio = width/height; 
+    while (window.isOpen())
+    {
+        if (Keyboard::isKeyPressed(Keyboard::Escape))
+        {
+            window.close();
+        }
 
-    VideoMode VideoWindow(width, height); 
-    RenderWindow window(VideoWindow,"Mandelbrot", Style::Default);
+        while (window.pollEvent(event))
+        {
+            if (event.type == Event::MouseButtonPressed)
+            {
 
+            }
+
+            if (event.type == Event::MouseMoved)
+            {
+
+            }
+
+            if (Keyboard::isKeyPressed(Keyboard::Escape))
+            {
+                window.close();
+            }
+        }
+    }
 
 
 
