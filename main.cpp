09986e00a24b46fda6f069046f0b0172761283f3
admin <admin@BCS-Ubuntu-01.lrccd.ad.losrios.edu>
2022-10-02 04:28:56 -0700

    New Changes

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
-#include <ComplexPlane.h>
+#include "ComplexPlane.h"
 
 using namespace std;
 using namespace sf;
@@ -12,7 +12,7 @@ int aspectRatio = height/width;
 VideoMode VideoWindow(width, height); 
 RenderWindow window(VideoWindow,"Mandelbrot", Style::Default);
 
-ComplexPlane complexPlane();
+ComplexPlane complexPlane(aspectRatio);
 Font font; 
 Text text; 
 VertexArray Colors(Points, height*width);
@@ -35,17 +35,44 @@ int main()
         {
             if (event.type == Event::MouseButtonPressed)
             {
+                Vector2f point = window.mapPixelToCoords(Mouse::getPosition(window), complexPlane.getView());
+
+                if (event.mouseButton.button == Mouse::Right)
+                {
+                    complexPlane.zoomOut();
+                    complexPlane.setCenter(point);
+                }
+
+                if (event.mouseButton.button == Mouse::Left)
+                {
+                    complexPlane.zoomIn();
+                    complexPlane.setCenter(point);
+                }
+
+                current = CALCULATING;
 
             }
 
             if (event.type == Event::MouseMoved)
             {
-
+                Vector2f point = window.mapPixelToCoords(Mouse::getPosition(window), complexPlane.getView());
+                complexPlane.setMouseLocation(point);
             }
 
             if (Keyboard::isKeyPressed(Keyboard::Escape))
             {
                 window.close();
+            }
+        }
+
+        if (current == CALCULATING)
+        {
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    Colors[j + i * width].position = Vector2f((float)i, (float)j);
+                }
             }
         }
     }
