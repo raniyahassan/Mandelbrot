a32157f4d989490f0aef1d963541e3dafdbfca16
admin <admin@BCS-Ubuntu-28.lrccd.ad.losrios.edu>
2022-10-03 22:27:00 -0700

    changes

--- code/ComplexPlane.cpp
+++ code/ComplexPlane.cpp
@@ -45,7 +45,7 @@ void ComplexPlane::setMouseLocation(Vector2f coord)
 void ComplexPlane::loadText(Text& text)
 {
     stringstream str; 
-    float centerX, centerY, mouseX, mouseY; 
+    double centerX, centerY, mouseX, mouseY; 
     str << m_view.getCenter().x << m_view.getCenter().y << m_mouseLocation.x << m_mouseLocation.y;
     str >> centerX >> centerY >> mouseX >> mouseY; 
     text.setString("Mandelbrot Set\nCenter: (" + to_string(centerX) + "," + to_string(centerY) + ")\n" + "Cursor: " + "(" + to_string(mouseX) + "," + to_string(mouseY) + ")\n" + "Left Click to Zoom In\nRight Click to Zoom Out");
@@ -73,31 +73,68 @@ size_t ComplexPlane::countIterations(Vector2f coord)
 
 void ComplexPlane::iterationsToRGB(size_t count, Uint8& r, Uint8& g, Uint8& b)
 {
-    if (count == MAX_ITER)
+    /*if (count == MAX_ITER)
+    {
+        r = 0;
+        g = 0;
+        b = 0; 
+    }
+
+    else if ((count%10)*5 > 53 )
+    {
+        r = 237;
+        g = 113;
+        b = 104;
+    }
+
+    else if ((count%10)*5 > 33 )
+    {
+        r = 239;
+        g = 144;
+        b = 160;
+    }
+
+    else if ((count%10)*5 > 20 )
+    {
+        r = 242;
+        g = 171;
+        b = 181;
+    }
+
+    else if ((count%10)*5 > 0)
+    {
+        r = 246;
+        g = 205;
+        b = 204;
+    }
+
+    */
+   if (count == MAX_ITER)
     {
         r = 0;
         g = 0;
         b = 0;
     }
 
-    else if ( count >50)
-    {
-        r =222-(count%17)*5;
-        g = 179-(count%17)*5;
-        b =173;
-    }
-    else if (count >30)
+    else if ( count > 50)
     {
         r = 222-(count%17)*5;
-        g=132-(count%17)*5;
-        b=123;
+        g = 179-(count%17)*5;
+        b = 173;
+    }
+    else if (count > 30)
+    {
+        r = 222-(count%17)*5;
+        g = 132-(count%17)*5;
+        b = 123;
 
     }
+
     else if (count > 15 )
     {
         
 
-        r=185-(count%17)*5;
+        r = 185-(count%17)*5;
         g = 92-(count%17)*5;
         b = 80;
     }
@@ -106,6 +143,6 @@ void ComplexPlane::iterationsToRGB(size_t count, Uint8& r, Uint8& g, Uint8& b)
         r = 59+(count%17)*5;
         g = 20;
         b = 40;  
-    }
+    } 
 }
 
--- code/main.cpp
+++ code/main.cpp
@@ -14,19 +14,22 @@ int main()
     Event event;
     VideoMode VideoWindow(width, height); 
     RenderWindow window(VideoWindow,"Mandelbrot", Style::Default);
+    RectangleShape rect(Vector2f{500 , 210}); 
+    rect.setFillColor(Color(0,0,0,100)); 
 
     ComplexPlane complexPlane(aspectRatio);
     Font font; 
-    if (!font.loadFromFile("font/JandaCurlygirlSerif.ttf"))
+    if (!font.loadFromFile("font/VCR_OSD_MONO.ttf"))
     {
         cout << "Error loading font!" << endl; 
     }
     Text text; 
     text.setFont(font); 
+    text.setLineSpacing(1.25);
     text.setCharacterSize(30); 
     text.setFillColor(Color::White);
     text.setString(""); 
-    text.setPosition(0,0); 
+    text.setPosition(5,5); 
 
     VertexArray Colors(Points, height*width);
 
@@ -69,7 +72,10 @@ int main()
 
             if (event.type == Event::MouseMoved)
             {
-                complexPlane.setMouseLocation(window.mapPixelToCoords(Mouse::getPosition(), complexPlane.getView()));
+                Vector2i mousePosition = Vector2i(event.mouseMove.x, event.mouseMove.y); 
+                Vector2f position = window.mapPixelToCoords(mousePosition, complexPlane.getView());
+                
+                complexPlane.setMouseLocation(position);
             }
 
         }
@@ -99,17 +105,18 @@ int main()
                 current = DISPLAYING;
             }
         }
-        }
+    
 
         if (current == DISPLAYING)
         {
             window.clear();
             window.draw(Colors);
+            window.draw(rect); 
             window.draw(text);
 
             window.display();  
         }
-
+    }
 
     return 0; 
 }
